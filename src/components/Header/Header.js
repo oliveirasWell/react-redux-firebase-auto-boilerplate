f449a2f547e207401156aacb229453bdf62e17b2
Wellington Oliveira <well.oliveira.snt@gmail.com>
2018-02-05 04:31:23 -0300

    Header with react-redux

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,33 +2,32 @@ import React from 'react';
 import Logout from "../Logout/Logout";
 import Fade from "../Fade/Fade";
 import PropTypes from "prop-types";
+import {connect} from "react-redux";
 
 const styles = {
     divAlert: {
         textAlign: 'center',
         color: 'red',
         fontWeight: '600'
+    },
+    div: {
+        textAlign: 'right'
     }
 };
 
-
 class Header extends React.Component {
-    state = {in: false, msg: '', userAuth:null};
+    state = {in: false, msg: '', userAuth: null};
 
     componentDidMount = () => {
         this.setState({in: true});
-        this.context.store.subscribe(() => this.setState({msg: this.context.store.getState().msg}));
-        this.context.store.subscribe(() => this.setState({userAuth: this.context.store.getState().userAuth}));
     };
 
     render = () => {
         return <Fade in={this.state.in}>
-            <div style={{textAlign: 'right'}}>
-
-                {this.state.msg != null && <div style={styles.divAlert}>{this.state.msg} </div>}
-
-                {!!this.state.userAuth &&
-                <span>{this.state.userAuth.displayName} <Logout/></span>}
+            <div style={styles.div}>
+                {this.props.msg != null && <div style={styles.divAlert}>{this.props.msg} </div>}
+                {!!this.props.userAuth &&
+                <span>{this.props.userAuth.displayName} <Logout/></span>}
             </div>
         </Fade>
     };
@@ -38,4 +37,11 @@ Header.contextTypes = {
     store: PropTypes.object.isRequired,
 };
 
-export default Header;
+const mapStateToProps = state => {
+    return {
+        userAuth: state.userAuth,
+        msg: state.msg
+    }
+};
+
+export default connect(mapStateToProps, null)(Header);
