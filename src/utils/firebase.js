56dce3b02c744cd20fe1945012972196c648940e
Wellington Oliveira <well.oliveira.snt@gmail.com>
2018-02-16 01:51:40 -0300

    env

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase';
 
-const config = {
+const prodConfig = {
     apiKey: "***************",
     authDomain: "***************",
     databaseURL: "***************",
@@ -8,6 +8,19 @@ const config = {
     storageBucket: "***************",
     messagingSenderId: "***************"
 };
+
+const devConfig = {
+    apiKey: "***************",
+    authDomain: "***************",
+    databaseURL: "***************",
+    projectId: "***************",
+    storageBucket: "***************",
+    messagingSenderId: "***************"
+};
+
+const config = process.env.NODE_ENV === 'production'
+    ? prodConfig
+    : devConfig;
 
 export const firebaseImpl = firebase.initializeApp(config);
 export const firebaseDatabase = firebase.database();
